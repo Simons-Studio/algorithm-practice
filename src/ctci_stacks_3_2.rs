8b624877ced2b54ec3378ae8d476a0aacee7fb4c
Simon Bennett <sim.j.bennett@gmail.com>
2024-03-05 16:28:42 +1100

    add rust

---
+++ src/ctci_stacks_3_2.rs
@@ -0,0 +1,13 @@
+pub fn main() {
+    // Some text here
+    let mut stack: Vec<Stack_Item>;
+}
+
+struct Stack {
+    stack: Vec<Stack_Item>
+}
+
+struct Stack_Item {
+    number: i32,
+    local_min: i32
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod ctci_stacks_3_2;
+
+fn main() {
+    ctci_stacks_3_2::main();
+}
