5cbd590b1f9237fbc9421f6ea751339a0886dd52
Simon Bennett <sim.j.bennett@gmail.com>
2024-03-09 21:24:46 +1100

    corrected pointer initiation

--- src/ctci_stacks_3_2.rs
+++ src/ctci_stacks_3_2.rs
@@ -3,11 +3,25 @@ pub fn main() {
     let mut stack: Stack;
 }
 
-struct Stack {
-    stack: Vec<StackItem>
+struct Stack<'a> {
+    stack: Vec<StackItem<'a>>
+}
+impl<'a> Stack<'a> {
+    fn pop(&mut self) -> Option<StackItem<'a>> {
+        if self.stack.is_empty() { None }
+        else {
+            let last = self.stack.last();
+
+            None
+        }
+    }
+
+    fn push(&mut self, number: i32) {
+
+    }
 }
 
-struct StackItem {
+struct StackItem<'a> {
     number: i32,
-    local_min: *const StackItem
+    local_min: Option<&'a StackItem<'a>>
 }
