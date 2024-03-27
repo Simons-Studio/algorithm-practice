01ac8fcc4007d114a74239d3e854059bee0e77f6
Simon Bennett <sim.j.bennett@gmail.com>
2024-03-27 13:19:00 +1100

    basic stack complete

--- src/ctci_stacks_3_2.rs
+++ src/ctci_stacks_3_2.rs
@@ -1,6 +1,14 @@
-pub fn main() {
+use core::fmt;
+use std::mem::take;
+
+pub fn run() {
     // Some text here
-    let mut stack: Stack;
+    let mut stack = Stack::from_vec(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+
+    stack.push(5);
+    stack.push(4);
+
+    println!("{}", stack)
 }
 
 #[derive(Debug)]
@@ -9,25 +17,38 @@ struct Stack {
     lenght: usize
 }
 impl<'a> Stack {
+    fn new() -> Stack {
+        Stack { top: None, lenght: 0 }
+    }
+
+    fn from_vec(numbers: Vec<i32>) -> Stack {
+        let mut result = Stack::new();
+
+        for n in numbers {
+            result.push(n);
+        }
+
+        result
+    }
+
     fn pop(&mut self) -> Option<i32> {
         let Some(top) = self.top.take() else {
             return None;
         };
 
         let value = top.number;
-
-        if let Some(next_box) = top.next {            
+        if let Some(next_box) = top.next {
             self.top = Some(*next_box);
         } else {
             self.top = None;
         }
 
         self.lenght -= 1;
-
         Some(value)
     }
 
     fn push(&mut self, number: i32) {
+        self.lenght += 1;
         if let Some(top) = self.top.take() {
             let node = StackNode { number, next: Some(Box::new(top)) };
             self.top = Some(node);
@@ -39,8 +60,33 @@ impl<'a> Stack {
     }
 }
 
+impl fmt::Display for Stack {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some(top) = &self.top {
+            let contents = top.string_contents();
+
+            write!(f, "{} | Length: {}", contents, self.lenght)
+        } else {
+            write!(f, "Empty Stack")
+        }
+    }
+}
+
 #[derive(Debug)]
 struct StackNode {
     number: i32,
     next: Option<Box<StackNode>>,
+}
+impl StackNode {
+    fn string_contents(&self) -> String {
+        let mut accumulator = String::new();
+        if let Some(next) = &self.next {
+            let next_str = next.string_contents();
+            accumulator.push_str(&next_str);
+            accumulator.push_str(" > ");
+        }
+        accumulator.push_str(&self.number.to_string());
+
+        accumulator
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ mod ctci_stacks_3_2;
 mod ctci_stacks_3_4;
 
 fn main() {
-    // ctci_stacks_3_2::main();
-    ctci_stacks_3_4::run();
+    let s = "3_2";
+
+    match s {
+        "3_2" => ctci_stacks_3_2::run(),
+        "3_4" => ctci_stacks_3_4::run(),
+        _ => println!("No exercise selected")
+    }
+    
+    
 }
