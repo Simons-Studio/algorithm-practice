caf41503764210c870b4c56e09072b419497bcfa
Simon Bennett <sim.j.bennett@gmail.com>
2024-03-27 13:28:44 +1100

    stack module

--- src/ctci_stacks_3_2.rs
+++ src/ctci_stacks_3_2.rs
@@ -1,4 +1,4 @@
-use core::fmt;
+use stack::Stack;
 
 pub fn run() {
     // Some text here
@@ -7,85 +7,93 @@ pub fn run() {
     stack.push(5);
     stack.push(4);
 
-    println!("{}", stack)
-}
+    println!("{}", stack);
 
-#[derive(Debug)]
-struct Stack {
-    top: Option<StackNode>,
-    lenght: usize
+    println!("Pop {:?}", stack.pop());
+
+    println!("{}", stack);
+
 }
-impl<'a> Stack {
-    fn new() -> Stack {
-        Stack { top: None, lenght: 0 }
+mod stack {
+    use core::fmt;
+
+    #[derive(Debug)]
+    pub struct Stack {
+        top: Option<StackNode>,
+        lenght: usize
     }
-
-    fn from_vec(numbers: Vec<i32>) -> Stack {
-        let mut result = Stack::new();
-
-        for n in numbers {
-            result.push(n);
+    impl<'a> Stack {
+        pub fn new() -> Stack {
+            Stack { top: None, lenght: 0 }
         }
 
-        result
-    }
+        pub fn from_vec(numbers: Vec<i32>) -> Stack {
+            let mut result = Stack::new();
 
-    fn pop(&mut self) -> Option<i32> {
-        let Some(top) = self.top.take() else {
-            return None;
-        };
+            for n in numbers {
+                result.push(n);
+            }
 
-        let value = top.number;
-        if let Some(next_box) = top.next {
-            self.top = Some(*next_box);
-        } else {
-            self.top = None;
+            result
         }
 
-        self.lenght -= 1;
-        Some(value)
-    }
+        pub fn pop(&mut self) -> Option<i32> {
+            let Some(top) = self.top.take() else {
+                return None;
+            };
 
-    fn push(&mut self, number: i32) {
-        self.lenght += 1;
-        if let Some(top) = self.top.take() {
-            let node = StackNode { number, next: Some(Box::new(top)) };
-            self.top = Some(node);
-        } else {
-            let node = StackNode { number, next: None }; 
-            self.top = Some(node);
-            return;
-        };
-    }
-}
+            let value = top.number;
+            if let Some(next_box) = top.next {
+                self.top = Some(*next_box);
+            } else {
+                self.top = None;
+            }
 
-impl fmt::Display for Stack {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if let Some(top) = &self.top {
-            let contents = top.string_contents();
+            self.lenght -= 1;
+            Some(value)
+        }
 
-            write!(f, "{} | Length: {}", contents, self.lenght)
-        } else {
-            write!(f, "Empty Stack")
+        pub fn push(&mut self, number: i32) {
+            self.lenght += 1;
+            if let Some(top) = self.top.take() {
+                let node = StackNode { number, next: Some(Box::new(top)) };
+                self.top = Some(node);
+            } else {
+                let node = StackNode { number, next: None }; 
+                self.top = Some(node);
+                return;
+            };
         }
     }
-}
 
-#[derive(Debug)]
-struct StackNode {
-    number: i32,
-    next: Option<Box<StackNode>>,
-}
-impl StackNode {
-    fn string_contents(&self) -> String {
-        let mut accumulator = String::new();
-        if let Some(next) = &self.next {
-            let next_str = next.string_contents();
-            accumulator.push_str(&next_str);
-            accumulator.push_str(" > ");
+    impl fmt::Display for Stack {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            if let Some(top) = &self.top {
+                let contents = top.string_contents();
+
+                write!(f, "{} | Length: {}", contents, self.lenght)
+            } else {
+                write!(f, "Empty Stack")
+            }
         }
-        accumulator.push_str(&self.number.to_string());
+    }
 
-        accumulator
+    #[derive(Debug)]
+    struct StackNode {
+        number: i32,
+        next: Option<Box<StackNode>>,
+    }
+    impl StackNode {
+        fn string_contents(&self) -> String {
+            let mut accumulator = String::new();
+            if let Some(next) = &self.next {
+                let next_str = next.string_contents();
+                accumulator.push_str(&next_str);
+                accumulator.push_str(" > ");
+            }
+            accumulator.push_str(&self.number.to_string());
+
+            accumulator
+        }
     }
 }
