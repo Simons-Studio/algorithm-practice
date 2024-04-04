b3c7a2526d9923f39b34dd96e331d75dbe348dae
Simon Bennett <sim.j.bennett@gmail.com>
2024-04-04 11:19:03 +1100

    main now runs 4.2

--- src/ctci_graphs_4/ctci_graphs_4_2.rs
+++ src/ctci_graphs_4/ctci_graphs_4_2.rs
@@ -1,6 +1,15 @@
-// Given a sorted array with unique integer elements, write an algorithm to 
+// Given a sorted array (ascending order) with unique integer elements, write an algorithm to 
 // create a binary search tree with minimal height
 
+use crate::utils::trees::BinarySearchTreeNode;
+
 pub fn run() {
-    
+    let x = 10..23;
+    let y = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    let mut tree = BinarySearchTreeNode::leaf(0);
+    for i in x {
+        tree.insertMax(i);
+    }
+    println!("{}", tree.to_string());
 } 
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ mod ctci_graphs_4;
 mod utils;
 
 fn main() {
-    let s = "4_1";
+    let s = "4_2";
 
     match s {
         "3_2" => ctci_stacks_3_2::run(),
         "3_4" => ctci_stacks_3_4::run(),
         "4_1" => ctci_graphs_4::ctci_graphs_4_1::run(),
+        "4_2" => ctci_graphs_4::ctci_graphs_4_2::run(),
         _ => println!("No exercise selected")
     }
     
